08c69532edbac183218556976144ff8dce14671a
Eleazar Saavedra <eleazarsaavedra@gmail.com>
2018-02-13 21:36:44 -0430

    Scroll infinito

---
+++ ScrollInf/ScrollInf/DataService.cs
@@ -0,0 +1,40 @@
+﻿//Clase que retornara o enviara la lista de Elementos
+//que seran mostrados en la ListView
+
+namespace ScrollInf
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class DataService
+    {
+        private readonly List<string> _data = new List<string>
+        {
+            "Mohamed", "Hassen", "Omar", "Ali", "Othman", "Adam", "Seif", "Hamed", "Paul",
+            "David", "Fehmi", "Badr", "Hamza", "Nabil", "Hajer", "Sami", "Ahmed", "Amir",
+            "Nebrass", "Karim", "Cherif", "Alaa", "Samar", "Narjess", "Iheb", "Safa",
+            "Mohamed", "Hassen", "Omar", "Ali", "Othman", "Adam", "Seif", "Hamed", "Paul",
+            "David", "Fehmi", "Badr", "Hamza", "Nabil", "Hajer", "Sami", "Ahmed", "Amir",
+        };
+
+        /*
+         Este metodo se encarga de asignar el retardo para mostrar los elementos
+         y
+         Mostrar ciertos elementos (indicados por parametros) y omitir el resto
+             */
+        public async Task<List<string>> GetItemsAsync(int pageIndex, int pageSize)
+        {
+            //asigna un retardo
+
+            await Task.Delay(2000);
+
+            //Skip omite una serie de elementos en una secuencia y devuelve los restantes
+
+            //Take Devuele un num especifico de elementos contiguos desde el
+            //principio de una secuencia
+
+            return _data.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+        }
+    }
+}
